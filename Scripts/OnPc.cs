0920bd75bcf699f0172913b39ac66f3c586416a4
Filipe <flipslv98@gmail.com>
2026-02-05 15:38:51 +0100

    racist folder FAH

--- Scripts/OnPc.cs
+++ Scripts/OnPc.cs
@@ -3,13 +3,18 @@ using System;
 
 public partial class OnPc : CanvasLayer
 {
+
+    [Export] public ColorRect Typing;
+    [Export] public ColorRect CharSelect;
     [Export] public Label Read;
     [Export] public Label Write;
     public override void _Ready()
     {
-        if(Read == null) Read = GetNode<Label>("Read");
-        if(Write == null) Write = GetNode<Label>("Write");
-        Read.Text = "Thongs to copy";
+        if(Typing == null) Typing = GetNode<ColorRect>("Typing");
+        if(CharSelect == null) CharSelect = GetNode<ColorRect>("CharSelect");
+        if(Read == null) Read = Typing.GetNode<Label>("Read");
+        if(Write == null) Write = Typing.GetNode<Label>("Write");
+        Read.Text = "Things to copy";
     }
 
     public override void _Process(double delta)
