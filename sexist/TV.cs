fba0e2a556faf48af2bce463035b33dee0fe1c1b
Filipe <flipslv98@gmail.com>
2026-02-26 15:10:26 +0100

    tv scene

--- sexist/TV.cs
+++ sexist/TV.cs
@@ -3,6 +3,20 @@ using System;
 
 public partial class TV : Node3D, IInteractable
 {
+    [Export] public MeshInstance3D Screen;
+    [Export] public Viewport SubViewport;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        if(Screen == null) Screen = GetNode<MeshInstance3D>("Screen");
+        if(SubViewport == null) SubViewport = Screen.GetNode<SubViewport>("SubViewport");
+
+        StandardMaterial3D screenMaterial = new StandardMaterial3D();
+        screenMaterial.AlbedoTexture = SubViewport.GetTexture();
+        Screen.MaterialOverride = screenMaterial;
+    }
+
     public void Interact(PlayerCharacter player)
     {
         player.ZoomToTV(this.Transform);
